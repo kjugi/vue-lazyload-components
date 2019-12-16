f5eabd7da5b5cf213ea059e31453da479adea540
Filip <filip.kudla@snow.dog>
2019-12-16 23:16:59 +0100

    Update exporting main script file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import MainComponent from './MainComponent.vue'
 
-export default {
-  install(Vue) {
-    // TODO: add support to global options in options.observerOptions
-    Vue.prototype.$VueLazyComponent = {
-      name: MainComponent.name
-    }
-
-    Vue.component(MainComponent.name, MainComponent)
+export default function install(Vue) {
+  // TODO: add support to global options in options.observerOptions
+  Vue.prototype.$VueLazyComponent = {
+    name: MainComponent.name
   }
+
+  Vue.component(MainComponent.name, MainComponent)
 }
