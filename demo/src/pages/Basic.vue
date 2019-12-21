59189b26bcd62cd6dd6071bfed200b936fdfc33f
Filip <filip.kudla@snow.dog>
2019-12-21 01:14:47 +0100

    Add Basic example page with lib

---
+++ demo/src/pages/Basic.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <vue-lazy-component
+      :options="options"
+      :is-loaded-prop="false"
+    >
+      <template v-slot:loader>
+        <div class="loader">
+          blah blah loader...
+        </div>
+      </template>
+
+      <img src="https://source.unsplash.com/random"/>
+    </vue-lazy-component>
+
+    <vue-lazy-component
+      :options="options"
+      :is-loaded-prop="false"
+    >
+      <template v-slot:loader>
+        <div class="loader">
+          blah blah loader...
+        </div>
+      </template>
+
+      <img src="https://source.unsplash.com/random"/>
+    </vue-lazy-component>
+
+    <vue-lazy-component
+      :options="options"
+      :is-loaded-prop="false"
+    >
+      <template v-slot:loader>
+        <div class="loader">
+          blah blah loader...
+        </div>
+      </template>
+
+      <img src="https://source.unsplash.com/random"/>
+    </vue-lazy-component>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      options: {
+        disableIntersection: true
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  max-width: 100%;
+}
+
+.loader {
+  height: 300px;
+  width: 100%;
+  text-align: center;
+}
+</style>
