5fc6be0b8f47826b332969855417c9d5d8b32f22
Filip <filip.kudla@snow.dog>
2019-12-21 01:14:04 +0100

    Add vue-router to demo page

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,12 +1,27 @@
 /* globals VueLazyComponent */
 
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+import MainApp from './App.vue'
+
+// Routes
+import Basic from './pages/Basic.vue'
+
 import './assets/vue-lazy-component.js'
 
+Vue.use(VueRouter)
 Vue.use(VueLazyComponent)
 
+const routes = [
+  {
+    path: '/basic',
+    component: Basic
+  }
+]
+
+const router = new VueRouter({ routes })
+
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  router,
+  render: h => h(MainApp)
+}).$mount('#app')
