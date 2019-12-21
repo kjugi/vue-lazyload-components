f382939e87300c60150e04164d93d808aac82991
Filip <filip.kudla@snow.dog>
2019-12-21 01:14:34 +0100

    Cleanup main demo component and homepage

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,59 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <vue-lazy-component :options="options" :is-loaded-prop="false">
-      <template v-slot:loader>
-        blah blah loader...
-      </template>
-      testing
-      <img src="https://picsum.photos/200/300"/>
-    </vue-lazy-component>
-    <vue-lazy-component :options="options" :is-loaded-prop="false">
-      <template v-slot:loader>
-        blah blah loader...
-      </template>
-      testing2
-      <img src="https://picsum.photos/200/300"/>
-    </vue-lazy-component>
-    <vue-lazy-component :options="options" :is-loaded-prop="false">
-      <template v-slot:loader>
-        blah blah loader...
-      </template>
-      testing3
-      <img src="https://picsum.photos/200/300"/>
-    </vue-lazy-component>
+    <h1 class="title">
+      Vue-lazyload-componenets demo
+    </h1>
+
+    <ul>
+      <li>
+        <router-link to="/basic">
+          Basic example
+        </router-link>
+      </li>
+
+      <li>
+        Example with never disabling intersection
+      </li>
+
+      <li>
+        Example with infinite images
+      </li>
+
+      <li>
+        Custom intersection options
+      </li>
+
+      <li>
+        <router-link to="/">Back to homepage</router-link>
+      </li>
+    </ul>
+
+    <!-- route outlet -->
+    <!-- component matched by the route will render here -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      options: {
-        disableIntersection: true,
-        intersectionOptions: {
-          threshold: 0.5
-        }
-      }
-    }
-  }
+  name: 'app'
 }
 </script>
 
 <style>
+body {
+  max-width: 800px;
+  margin: auto;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
