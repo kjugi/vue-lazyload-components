29db71cdd5d10537b4d4e36a96ffff2197006f6a
Filip <filip.kudla@snow.dog>
2020-01-01 17:21:00 +0100

    Write new test cases to content visibility, emmiting events, testing options

--- tests/unit/index.spec.js
+++ tests/unit/index.spec.js
@@ -44,17 +44,71 @@ describe('Testing base functions', () => {
 </div>`)
   })
 
-  // it('Should change main tag of component when pass option', () => {
+  it('Should change main tag of component when pass option', () => {
+    const wrapper = mount({
+      components: {
+        VueLazyComponent
+      },
+      template: `
+        <div>
+          <vue-lazy-component main-tag="div"></vue-lazy-component>
+        </div>
+      `
+    })
 
-  // })
+    expect(wrapper.isVueInstance()).toBe(true)
+    expect(wrapper.html()).toBe(`<div>
+  <div><span>
+        Loading...
+      </span></div>
+</div>`)
+  })
 
-  // it('Content should be visible on render', () => {
+  it('Content should be visible on render', async () => {
+    const wrapper = mount({
+      components: {
+        VueLazyComponent
+      },
+      template: `
+        <div>
+          <vue-lazy-component :is-loaded="true">Testing slot content</vue-lazy-component>
+        </div>
+      `
+    })
 
-  // })
+    expect(wrapper.vm._vnode.children[0].componentInstance.observer.observables.length).toBe(1)
 
-  // it('Should emit event when component is intersected', async () => {
+    expect(wrapper.vm.$el.outerHTML).toBe(`<div><section>Testing slot content</section></div>`)
+    expect(wrapper.vm.$el.textContent).toBe('Testing slot content')
+  })
 
-  // })
+  it('Should emit event when component is intersected', async () => {
+    const intersect = jest.fn()
+    const wrapper = mount({
+      components: {
+        VueLazyComponent
+      },
+      template: `
+        <div>
+          <vue-lazy-component
+            :is-loaded="true"
+            v-on:custom-lazy-component="eventCatcher"
+          >
+            Testing slot content
+          </vue-lazy-component>
+        </div>
+      `,
+      methods: {
+        eventCatcher: intersect
+      }
+    })
+
+    wrapper.vm._vnode.children[0].componentInstance.observer.status([{
+      isIntersecting: true
+    }])
+
+    expect(intersect).toHaveBeenCalledTimes(1)
+  })
 
   // it('Should emit event every time when component is intersected', async () => {
 
